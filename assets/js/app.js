70bca080c39b2ba7aff2d41d8c1e491e5a386050
Shinchan54 <91662751+Gojo54@users.noreply.github.com>
2022-12-21 16:23:25 +0530

    jokes

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+const p = document.querySelector('p');
+const button = document.querySelector('button');
+
+const url = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`
+
+button.addEventListener('click', e=>{
+  p.classList.remove('fade')
+  fetch(url)
+  .then(data => data.json())
+  .then(item =>{
+     p.textContent = `${item.joke}`;
+     p.classList.add('fade')
+  });
+})
+
+
+
+
+
+
+
+
+
+//
