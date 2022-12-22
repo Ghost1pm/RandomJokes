decc2216a3f79481689844d1096ed123d998f2ea
Gojo <91662751+Gojo54@users.noreply.github.com>
2022-12-22 15:17:14 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,17 @@ const button = document.querySelector('button');
 const url = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`
 
 
-button.addEventListener('click', jokes() ) 
-
-
-
-function jokes(e){
+button.addEventListener('click', e=>{
   p.classList.remove('fade')
   fetch(url)
   .then(data => data.json())
   .then(item =>{
      p.textCpontent = `${item.joke}`;
      p.classList.add('fade')
-  });
- } 
+ }) 
+
+
+
 
 
 
